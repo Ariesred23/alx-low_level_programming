e228812636694aa8fee0ebd790ac995c2fb6bbf4
Nkechi <ariesgreennigerialtd@gmail.com>
2022-12-08 12:26:18 -0800

    program phrase

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *main - prints phrase "Programming is like building a multilingual puzzle
- *return : 0 if exited properly
- */
+*main - prints phrase "Programming is like building a multilingual puzzle
+*return : 0 if exited properly, no zero otherwise
+*/
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
