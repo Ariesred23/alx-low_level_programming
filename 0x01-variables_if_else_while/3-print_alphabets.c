21fc9246b40a0744a3d41857ac339b8987e56f60
Nkechi <ariesgreennigerialtd@gmail.com>
2022-12-11 07:46:57 -0800

    upper and lower

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,11 +12,11 @@ char alphabet;
 for (alphabet = 'a'; alphabet <= 'z'; alphabet++);
 {
 putchar(alphabet);
-}
+
 for (alphabet = 'A'; alphabet <= 'Z'; alphabet++);
-{
+
 putchar(alphabet);
-}
+
 putchar('\n');
 return (0);
 }
